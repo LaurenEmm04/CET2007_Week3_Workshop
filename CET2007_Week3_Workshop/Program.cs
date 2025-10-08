973b048b2ebca622043a031ba370a94999116ca6
lauren em <bi76os@student.sunderland.ac.uk>
2025-10-08 12:51:02 +0100

    Completed Task 1

--- CET2007_Week3_Workshop/Program.cs
+++ CET2007_Week3_Workshop/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CET2007w3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Meal meal = new Meal("Burger", 3.99m);
+            Drink drink = new Drink("Cola", 1.50m, "500ml");
+
+            meal.Describe();
+            drink.Describe();
+        }
+    }
+
+
+
+
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual void Describe()
+        {
+            Console.WriteLine($"{Name} - £{Price:F2}"); //prints burger - £3.99 Cola - £1.50
+        }
+    }
+
+    public class Meal : MenuItem
+    {
+        public Meal(string name, decimal price) : base(name, price) { }
+        public override void Describe()  //when overritten, prints
+        {
+            Console.WriteLine($"Meal: {Name} – £{Price:F2}");  //Meal: Burger - £3.99
+        }
+    }
+
+    public class Drink : MenuItem
+    {
+        public string Volume { get; set; }
+        public Drink(string name, decimal price, string volume) : base(name, price)
+        {
+            Volume = volume;
+        }
+
+        public override void Describe()  //this is the drinks part thats also overritten
+        {
+            Console.WriteLine($"Drink: {Name} ({Volume}) - £{Price:F2}");  // turns into Drink: Cola: (500ml) - £1.50
+        }
+    }
+}
